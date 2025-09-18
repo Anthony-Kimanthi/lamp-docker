fbbb71671b78449ea8b6c21979ec5ebf2aaae57a
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-18 14:30:15 -0700

    Update and rename Dockerfile.php to Dockerfile

--- php/Dockerfile.php
+++
@@ -1,7 +0,0 @@
-FROM php:7.4-apache
-
-# Install mysqli and pdo_mysql
-RUN docker-php-ext-install mysqli pdo pdo_mysql && docker-php-ext-enable mysqli pdo_mysql
-
-# Optional: enable Apache mod_rewrite if needed
-RUN a2enmod rewrite
