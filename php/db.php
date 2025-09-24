439c48e9875bff40dd729461d042c25822eacd13
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-24 10:48:27 -0700

    Update db.php

--- php/db.php
+++ php/db.php
@@ -1,7 +1,7 @@
 <?php
 $host = "mysql_db";       // same as in docker-compose service name
 $user = "bloguser";      // your non-root user
-$pass = "blogpassword";  // the password you set in docker-compose
+$pass = "blogpass";  // the password you set in docker-compose
 $db   = "blogdb";         // your database name
 
 $conn = new mysqli($host, $user, $pass, $db);
