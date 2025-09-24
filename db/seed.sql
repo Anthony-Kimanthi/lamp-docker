10bac036c768c572ed983ff6e60c09e95e3d0e09
Anthony-Kimanthi <108864059+Anthony-Kimanthi@users.noreply.github.com>
2025-09-24 11:15:03 -0700

    Create seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+-- db/seed.sql
+INSERT INTO posts (title, content) VALUES
+('Hello World!', 'This is your first blog post.'),
+('Docker + PHP + MySQL', 'Your LAMP stack blog is running inside Docker!'),
+('Docker + PHP + MySQL', 'Running a blog using containers is fun!');
